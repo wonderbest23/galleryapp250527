a4809522570f6fc325bedbc18ffbc858a762dcf4
wonderbest23 <168019437+wonderbest23@users.noreply.github.com>
2025-06-16 12:42:32 +0900

    네이버 사이트 소유 인증 메타태그 추가

--- app/head.js
+++ app/head.js
@@ -3,6 +3,7 @@ export default function Head() {
     <>
       {/* iPhone Safari 포함 모든 모바일에서 화면 폭에 맞춰 줍니다 */}
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
+      <meta name="naver-site-verification" content="bad7ac6c90f1c11f1dfa8f9ef1d8bb44d3fc597c" />
     </>
   );
 } 
