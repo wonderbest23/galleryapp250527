3407a4e7511dabdd13ffa913f411112f6eb3f3ee
wonderbest23 <168019437+wonderbest23@users.noreply.github.com>
2025-06-18 07:49:59 +0900

    아이콘(favicon, apple-touch-icon 등) 메타태그 추가

--- app/head.js
+++ app/head.js
@@ -5,6 +5,13 @@ export default function Head() {
       {/* iPhone Safari 포함 모든 모바일에서 화면 폭에 맞춰 줍니다 */}
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
       <meta name="naver-site-verification" content="bad7ac6c90f1c11f1dfa8f9ef1d8bb44d3fc597c" />
+
+      {/* 아이콘 관련 메타태그 추가 */}
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      {/* <link rel="manifest" href="/site.webmanifest" /> */}
     </>
   );
 } 
