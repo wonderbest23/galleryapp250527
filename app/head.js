67c92766b8a3a959023221f6a47a85b012a4b101
wonderbest23 <168019437+wonderbest23@users.noreply.github.com>
2025-06-18 07:52:24 +0900

    twitter:title, og:title, og:site_name 등 사이트명을 미술예술랭으로 변경

--- app/head.js
+++ app/head.js
@@ -12,6 +12,11 @@ export default function Head() {
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       {/* <link rel="manifest" href="/site.webmanifest" /> */}
+
+      {/* Open Graph & Twitter 메타태그 */}
+      <meta property="og:title" content="미술예술랭" />
+      <meta property="og:site_name" content="미술예술랭" />
+      <meta name="twitter:title" content="미술예술랭" />
     </>
   );
 } 
