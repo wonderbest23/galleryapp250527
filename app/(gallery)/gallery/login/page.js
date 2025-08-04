05b8fd5251caaf00ad58a7ea06beb034430e2d58
wonderbest23 <168019437+wonderbest23@users.noreply.github.com>
2025-08-04 19:21:08 +0900

    이메일 주소를 support@artandbridge.com으로 변경

--- app/(gallery)/gallery/login/page.js
+++ app/(gallery)/gallery/login/page.js
@@ -57,7 +57,7 @@ export default function App({searchParams}) {
       <div className="flex flex-col gap-4 md:max-w-[30%] w-full max-w-[80%]">
         <h1 className="text-2xl font-bold text-center">갤러리 로그인</h1>
         <div className="text-center">
-            <p>이용문의 : rena35200@gmail.com </p>
+            <p>이용문의 : support@artandbridge.com </p>
             <a
               href="http://pf.kakao.com/_sBnXn/chat?mode=chat&input=%EC%95%84%EC%9D%B4%EB%94%94%EC%99%80%20%EB%B9%84%EB%B0%80%EB%B2%88%ED%98%B8%EB%A5%BC%20%EC%9E%8A%EC%96%B4%EB%B2%84%EB%A6%AC%EC%85%A8%EB%82%98%EC%9A%94%3F%20%EA%B0%A4%EB%9F%AC%EB%A6%AC%EB%AA%85%EC%9D%84%20%EB%82%A8%EA%B2%A8%EC%A3%BC%EC%84%B8%EC%9A%94."
               target="_blank"
